829af3b2233804f3521ec2e2155b56a83d49ad98
HardyZhan <hardy1235@163.com>
2023-03-13 15:44:50 +0800

    利用切面类向客户端返回新令牌

---
+++ src/main/java/com/hardyz/emos/wx/aop/TokenAspect.java
@@ -0,0 +1,35 @@
+package com.hardyz.emos.wx.aop;
+
+import cn.hutool.core.util.StrUtil;
+import com.hardyz.emos.wx.common.util.R;
+import com.hardyz.emos.wx.config.shiro.ThreadLocalToken;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TokenAspect {
+    @Autowired
+    ThreadLocalToken threadLocalToken;
+
+    @Pointcut("execution(public * com.hardyz.emos.wx.controller.*.*(..))")
+    public void aspect() {
+
+    }
+
+    @Around("aspect()")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        R r = (R)point.proceed();
+        String token = threadLocalToken.getToken();
+        // 如果ThreadLocalToken里面有token的值，说明是更新的token
+        if (token != null) {
+            r.put("token", token);
+            threadLocalToken.clear();
+        }
+        return r;
+    }
+}
